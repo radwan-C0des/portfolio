3f27b823c768b0480569bd5632e6e1c660e3d527
CodeWithRadwan <radoanbro1@gmail.com>
2026-01-09 01:12:22 +0600

    02:dock dock animation

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Navbar from '#components/Navbar.jsx'
 import Welcome from '#components/Welcome.jsx'
+import Dock from '#components/Dock.jsx'
 import ShaderBackground  from '#components/ShaderBackground.jsx'
 
-export{Navbar,Welcome,ShaderBackground};
+
+export{Navbar,Welcome,ShaderBackground,Dock};
