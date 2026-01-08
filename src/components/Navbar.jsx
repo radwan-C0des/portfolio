298abf9fb214e7cfdf2721d2dff71435638112f2
CodeWithRadwan <radoanbro1@gmail.com>
2026-01-08 15:51:21 +0600

    First commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import dayjs from 'dayjs'
+
+import { useState, useEffect } from 'react';
+
+import { navLinks,navIcons } from '#constants'
+import React from 'react'
+
+function Navbar() {
+    const [time, setTime] = useState(dayjs().format("ddd MMM D h:mm A"));
+
+     useEffect(() => {
+    
+    const timer = setInterval(() => {
+      
+      setTime(dayjs().format("ddd MMM D h:mm A")); 
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+    
+  return (
+    <nav>
+        <div>
+            <img src='/images/logo.svg' alt='logo' />
+            <p className='font-bold'>Radwan's Portfolio</p>
+            <ul>
+            {navLinks.map((item) => (
+                <li key={item.id} >
+                    <p>{item.name}</p>
+                </li>
+            ) )}
+            </ul>
+        </div>
+        
+        <div>
+            <ul>
+            {
+                navIcons.map((item) => (
+                    <li key={item.id}>
+                        <img src={item.img} className='icon-hover' alt={`icon-${item.id}`}  />
+                    </li>
+                ))
+            }
+            </ul>
+            <time>{time}</time>
+        </div>
+        
+    </nav>
+  )
+}
+
+export default Navbar
